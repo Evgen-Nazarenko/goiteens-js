79c44427a081b770abc700cb684307716053be77
evgen nazarenko <nazarenkoevgen99@gmail.com>
2021-02-16 21:24:46 +0200

    Practical-5

---
+++ Practical-5/JS/script.js
@@ -0,0 +1,61 @@
+'use strict';
+//1
+
+let d = 1;
+
+do {
+
+
+    console.log(` ${d}`);
+    d += 1;
+} while (d < 11);
+
+
+let pass = '',
+    count = 0;
+
+console.log('---------------');
+
+
+//2
+
+
+ for (let i = 2; i <= 10; i++) {
+     if (i % 2 == 0) {
+       alert('Парне число ' + i);
+     }
+   }
+
+console.log('------------');
+//3
+let num1 = 20;
+
+do {
+    console.log( `${num1}` );
+    num1 +=1;
+} while (num1 < 29);
+
+
+
+//4
+
+let name = prompt('Напиши ім*я улюбленого героя!! ');
+
+if (name.length <6) {
+    console.log('Ім*я твого улюбленого героя є ' + name);
+}
+
+else {
+    console.log('УПС');
+}
+for(let g = 0; g > 10; ++g){
+    let j = g%2;
+    if(j = 0){
+     console.log('Число парне');
+    }
+    else{
+    console.log('Число непарне');
+    }
+}
+
+
